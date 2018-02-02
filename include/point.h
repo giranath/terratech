53e41b147d90db51c56465ddb427af3f385ff06d
naud2201 <dominic.nault@usherbrooke.ca>
2018-02-01 20:16:21 -0500

    fix point header #19

--- include/point.h
+++ include/point.h
@@ -1,16 +1,24 @@
 #ifndef DEF_POINT_H
 #define DEF_POINT_H
-#include <cmath>
+
 
 template <class T>
 struct point
 {
     T x;
     T y;
+    point() :x{}, y{}{}
+    point(T x, T y) :x{x}, y{y} {}
 
-    point() : x(T{}), y({}) {}
-    point(T x, T y) : x(x), y(y) {}
+    auto operator+(const point<T>& p)
+    {
+        return point(x + p.x, y + p.y);
+    }
 
+    bool operator==(const point<T>& p)
+    {
+        return x == p.x && y == p.y;
+    }
 };
-#endif // !DEF_POINT_H
+#endif
 
