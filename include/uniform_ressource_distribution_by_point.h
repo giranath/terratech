29d8fe186bc5d878b2314e8c1e35d7cbd5cda216
naud2201 <dominic.nault@usherbrooke.ca>
2018-02-06 21:10:41 -0500

    fix de uniform_ressource_distribution_by_point #19

--- include/uniform_ressource_distribution_by_point.h
+++ include/uniform_ressource_distribution_by_point.h
@@ -10,18 +10,17 @@
 
 class uniform_ressource_distribution_by_point
 {
-    using site_id_type = uint16_t;
     using dimension_type = size_t;
-    std::vector<site_id_type> sites_id;
+    std::vector<site_type> sites_id;
     uniform_point_distribution<dimension_type> point_distribution;
 
 public:
     uniform_ressource_distribution_by_point() = delete;
-    uniform_ressource_distribution_by_point(std::vector<site_id_type> sites_id, const dimension_type& width, const dimension_type& height) :
+    uniform_ressource_distribution_by_point(std::vector<site_type> sites_id, const dimension_type& width, const dimension_type& height) :
         sites_id{ sites_id.begin(), sites_id.end()},
         point_distribution{0, 0, width, height}{}
 
-    std::pair<point<dimension_type>, site_id_type> operator()(std::default_random_engine& engine) {
+    std::pair<point<dimension_type>, site_type> operator()(std::default_random_engine& engine) {
         std::uniform_int_distribution<dimension_type> sites_id_distribution{ 0, sites_id.size() - 1 };
 
         return { point_distribution(engine), sites_id[sites_id_distribution(engine)] };
